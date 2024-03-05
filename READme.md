ce7f416014911433aac6aa48a30ef5c3ccc8d750
Aiden Wahed <prismhead26@gmail.com>
2024-03-05 11:21:35 -0700

    first commit

---
+++ READme.md
@@ -0,0 +1,45 @@
+# Backend Note Builder
+  
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  
+  ## Description:
+  Your assignment is to modify starter code to create an application called Note Taker that can be used to write and save notes. This application will use an Express.js back end and will save and retrieve note data from a JSON file.The application’s front end has already been created. It's your job to build the back end, connect the two, and then deploy the entire application to Heroku.
+  
+  ## Table of Contents:
+  - [Installation Instructions](#Installation-Instructions)
+  - [Usage Information](#Usage-Instructions)
+  - [Links](#Links)
+  - [Features](#Features)
+  - [Contribution Guidelines](#Contribution-Guidelines)
+  - [Test Instructions](#Test-Instructions)
+  - [License](#License)
+  - [Questions](#Questions)
+
+  ## Installation Instructions:
+  
+  
+  ## Usage Instructions:
+  Step 1: Step 2: Step 3: Step 4: Step 5:
+
+  ## Links
+
+  ## Features
+  Here you will find the list of all npm/node packages used: 
+  
+  ## Contribution Guidelines:
+  
+  
+  ## Test Instructions:
+  test examples and how to
+  
+  ## License:
+  
+  Licensed under the The MIT License license.
+  
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)  (https://opensource.org/licenses/MIT)
+  
+  ## Questions:
+  For additonal questions or concerns, feel free to contact me via [prismhead26@gmail.com](prismhead26@gmail.com). 
+  You can also find me on Github at [prismhead26](https://github.com/prismhead26).
+  
+  © 2024 Aiden Wahed. All Rights Reserved.
---
+++ server.js
@@ -0,0 +1,99 @@
+const express = require('express')
+const morgan = require('morgan')
+const notes = require('./db/db.json')
+const path = require('path')
+const { readFile, writeFile } = require('fs/promises')
+const { uuid, jsonSchema, v4 } = require('uuid');
+
+const PORT = 3002
+
+const app = express()
+
+// middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(morgan('dev'))
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+})
+
+app.get('/api/notes', (req, res) => {
+    // Send a message to the client
+    res.status(200).json(notes);
+    // Log our request to the terminal
+    console.info(`${req.method} request received to get notes`);
+})
+
+app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} request received to get notes`)
+
+    const { title, text } = req.body
+
+    if ( title && text ) {
+        const newNote = {
+            title,
+            text,
+            id: v4()
+        }
+
+        readFile('./db/db.json').then((raw_data) => {
+            return JSON.parse(raw_data)
+        })
+        .then(data => {
+            const notes = (data);
+            notes.push(newNote);
+            writeFile('./db/db.json', JSON.stringify(notes))
+        })
+        .then(() => console.log('Success!'))
+        .catch(err => console.error(err));
+
+        const response = {
+            status: 'success',
+            body: newNote
+        }
+
+        console.log(response)
+        res.status(201).json(response)
+    } else {
+        res.status(500).json('Error in posting note')
+    }
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+
+    // before
+    var before_length = 0
+    var after_length = 0    
+    readFile('./db/db.json').then((raw_data) => {
+        return JSON.parse(raw_data);
+    })
+    .then(notes => {
+        before_length = notes.length
+        notes = notes.filter(({ id }) => id != req.params.id);
+        after_length = notes.length
+        return writeFile('./db/db.json', JSON.stringify(notes))
+    })
+    .then(() => console.log('DELETE Success!'))
+    .then(() => {
+        if (before_length == after_length) {
+            return res.sendStatus(404)
+        }
+        return res.sendStatus(200)
+    })
+    .catch(err => console.error(err));
+    // res.json('post complete')
+    // after
+})
+
+// 404 page
+app.use((req, res) => {
+    res.status(404).sendFile('./public/404.html', { root: __dirname })
+})
+
+app.listen(PORT, (err) => {
+    if (err) console.log(err)
+    console.log(`Listening on port ${PORT}...`)
+})
